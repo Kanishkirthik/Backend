e9f81c2f57478bde9787c82798db9513c45095ab
Kanish Kirthik K <129273259+Kanishkirthik@users.noreply.github.com>
2024-02-21 23:35:28 +0530

    Add files via upload

--- models/Profile.js
+++ models/Profile.js
@@ -1,7 +1,8 @@
 const mongoose=require('mongoose');
 const ProfileSchema=new mongoose.Schema({
     Username:{type:String, required:true,unique:true},
-    Role:{type:String},
+    Uid:{type:String, required:true,unique:true},
+    Role:{type:String,default: "Mentor"},
     Photourl:{type:String,required:true,unique:true},
     EnrolledCourses:{type:String,value:[]},
     CompletedCourses:{type:String,value:[]},
--- models/user.js
+++ models/user.js
@@ -1,10 +1,11 @@
 const mongoose =require('mongoose');
  const user=new mongoose.Schema({
     Username:{type:String,required:true,unique:true},
-    Password:{type:String,required:true},
+    Email:{type:String,required:true,unique:true},
     Uid:{type:String,required:true,unique:true},
-    Role:{type:String,required:true},
-
+    Password:{type:String},
+    Role:{type:String,default: "Mentor"},
+    Photourl:{type:String,required:true,unique:true},
  });
  const usermodel=mongoose.model("User",user);
  module.exports=usermodel;
